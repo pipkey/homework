d42318fc5c3ab1ee74f20cb5284c4310bf18aa85
pipkey <galagen2014@gmail.com>
2021-01-26 19:25:34 +0300

    5 HW

---
+++ src/p2-homeworks/h5/pages/Junior+.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function JuniorPlus () {
+    return (
+        <div>
+            <h1>Bye bye</h1>
+        </div>
+    );
+}
+
+export default JuniorPlus;
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Junior() {
+    return (
+        <div>
+            <h1>Hello!</h1>
+
+        </div>
+    );
+}
+
+export default Junior;
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
