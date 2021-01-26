9e1f817e81791e09b9e05e3b4ab225a56f6454f8
pipkey <galagen2014@gmail.com>
2021-01-26 20:41:40 +0300

    7 HW

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import HW7 from "../../h7/HW7";
 
 function Junior() {
     return (
         <div>
-            <h1>Hello!</h1>
-
+            <HW7/>
         </div>
     );
 }
